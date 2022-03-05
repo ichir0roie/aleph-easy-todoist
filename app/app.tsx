505fec663d21e31e4822feeb5d121fbe776486cc
ichir0roieHome <ichir0roie.com>
2022-03-05 13:58:52 +0000

    update icon

--- app/app.tsx
+++ app/app.tsx
@@ -10,6 +10,7 @@ export default function App({ Page, pageProps }: { Page: FC, pageProps: Record<s
     <main>
       <head>
         <meta name="viewport" content="width=device-width" />
+        <link rel="icon" href="logo.svg" type="image/svg+xml"/>
       </head>
       <Page {...pageProps} />
     </main>
