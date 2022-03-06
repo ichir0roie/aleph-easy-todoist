457fc6516f7ba8f4b98b33f074c2674a906b6e8d
ichir0roieHome <ichir0roie.com>
2022-03-06 13:05:22 +0000

    bug fix

--- app/app.tsx
+++ app/app.tsx
@@ -12,7 +12,7 @@ export default function App({ Page, pageProps }: { Page: FC, pageProps: Record<s
     <main>
       <head>
         <meta name="viewport" content="width=device-width" />
-        <link rel="icon" href="logo.svg" type="image/svg+xml"/>
+        <link rel="icon" href="/logo.svg" type="image/svg+xml"/>
       </head>
         <Page {...pageProps} />
       
